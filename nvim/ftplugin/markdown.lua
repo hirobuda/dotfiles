b7a83e35fb3eea3f2f407fc82383340fe56b38be
Hiroshi Ito <49350438+hirobuda@users.noreply.github.com>
2022-06-17 13:50:04 -0300

    Markdown ftplugin, now in Lua

---
+++ nvim/ftplugin/markdown.lua
@@ -0,0 +1,28 @@
+-- insert mode remaps
+norm_table = {'(', ')', '[', ']', '\\part', '\\the', '\\alp', '\\bet', '\\fr', '\\sum', '^', '_', '\\pd', '\\lim'}
+math_table = {'\\left(', '\\right)', '\\left[', '\\right]', '\\partial', '\\theta'
+, '\\alpha', '\\beta', '\\frac{}{}', '\\sum_{}^{}', '^{}', '_{}', '\\frac{\\partial}{\\partial}', '\\lim_{\\to}'}
+keymap_table = {}
+
+for i=1,14 do
+	keymap_table[norm_table[i]] = math_table[i] 
+end
+
+function isMath(k)
+ 	local groups = " "
+	for _,val in pairs(vim.fn.synstack(vim.fn.line("."), vim.fn.col("."))) do
+		groups = groups .. vim.fn.synIDattr(val, "name") .. " "
+	end
+	if string.find(groups, "Math") ~= nil then
+		return keymap_table[k]
+	end
+	return k
+end
+
+for i = 1,14 do
+	vim.keymap.set('i', norm_table[i], function() return isMath(norm_table[i]) end, {buffer=true, expr=true})
+end
+
+--normal mode remaps:
+
+vim.keymap.set('n', '<F2>', 'i$$<Enter>\\begin{aligned}<Enter><Enter>\\end{aligned}<Enter>$$', {buffer=true})
